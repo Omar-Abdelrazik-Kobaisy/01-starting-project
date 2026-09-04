8a58a4273dd3bbf0a57a88f997c37a5f45c27229
omarAbdelrazik <omar.abdelrazik98@gmail.com>
2026-09-04 18:51:00 +0300

    Introduce Tabs component and refactor Examples
    Add a small Tabs component to Examples.jsx and refactor Examples to build tabButtons and tabContent as variables. Replace the previous inline menu and content markup with <Tabs tabButtons={tabButtons}>{tabContent}</Tabs>. No functional change; improves readability and separation of concerns. Keys continue to use crypto.randomUUID().

--- src/Components/Sections/ExamplesSection/Examples.jsx
+++ src/Components/Sections/ExamplesSection/Examples.jsx
@@ -5,6 +5,15 @@ import TabContent from '../../TabContent/TabContent.jsx';
 import TAB_BUTTONS from '../../TabButton/data.js';
 import EXAMPLES from '../../TabContent/data.js';
 
+const Tabs = ({tabButtons, children}) => {
+    return (
+        <>
+            <menu>{tabButtons}</menu>
+            {children}
+        </>
+    );
+};
+
 const Examples = () => {
     const [clickedButton, setClickedButton] = useState('components');
 
@@ -13,23 +22,28 @@ const Examples = () => {
         setClickedButton(item);
         console.log(clickedButton);
     };
+
+    const tabButtons = TAB_BUTTONS.map((item) => (
+        <TabButton
+            key={crypto.randomUUID()}
+            onClick={() => handleClick(item.toLowerCase())}
+            isSelected={clickedButton === item.toLowerCase()}
+        >
+            {item}
+        </TabButton>
+    ));
+    const tabContent = (
+        <div id='tab-content'>
+            <TabContent
+                key={crypto.randomUUID()}
+                {...EXAMPLES[clickedButton]}
+            />
+        </div>
+    );
+
     return (
         <Section title='Examples' id='examples'>
-            <menu>
-                {TAB_BUTTONS.map((item) => (
-                    <TabButton
-                        key={crypto.randomUUID()}
-                        onClick= {() => handleClick(item.toLowerCase())}
-                        isSelected={clickedButton === item.toLowerCase()}>
-                        {item}
-                    </TabButton>
-                ))}
-            </menu>
-            <div id='tab-content'>
-                <TabContent
-                    key={crypto.randomUUID()}
-                    {...EXAMPLES[clickedButton]} />
-            </div>
+            <Tabs tabButtons={tabButtons}>{tabContent}</Tabs>
         </Section>
     );
 };
